26024c987f6cd75e19b0d725a90666d7d3c866da
groide754 <george_rodier@comcast.com>
2020-09-07 21:10:36 -0400

    Initial commit of pipeline

---
+++ bin/cdkpipelines-demo.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new cdk.App();
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: {
+    account: '381070733929',
+    region: 'us-east-2',
+  },
+});
